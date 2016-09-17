6efdfabffa0160657703c55d311a7aa1939f370a
Alexis Anand <alexis@alexisanand.com>
2016-09-17 11:18:02 -0700

    Fix server creation

--- game-server/index.js
+++ game-server/index.js
@@ -2,7 +2,7 @@
 
 const socketIo = require('socket.io');
 
-const server = new socketIo.Server();
+const server = new socketIo();
 
 server.on('connection', function(socket) {
     console.log('hello world');
